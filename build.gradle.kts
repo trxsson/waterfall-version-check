a945790f493c5f58850ddb53c32f6b4fa339a21e
trxsson <mail@trxsson.dev>
2023-03-19 06:20:54 +0100

    Added all required dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("java")
+    java
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "dev.trxsson"
@@ -7,13 +8,25 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    mavenLocal()
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
+    implementation("dev.trxsson:simple-configs:1.0.1-SNAPSHOT")
+    implementation("net.kyori:adventure-api:4.13.0")
+    implementation("net.kyori:adventure-text-serializer-legacy:4.13.0")
+    implementation("net.kyori:adventure-text-minimessage:4.13.0")
+    compileOnly("io.github.waterfallmc:waterfall-api:1.19-R0.1-SNAPSHOT")
+    compileOnly("org.jetbrains:annotations:24.0.1")
+    annotationProcessor("org.jetbrains:annotations:24.0.1")
+    compileOnly("org.projectlombok:lombok:1.18.26")
+    annotationProcessor("org.projectlombok:lombok:1.18.26")
 }
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+tasks {
+    jar {
+        dependsOn("shadowJar")
+    }
 }
